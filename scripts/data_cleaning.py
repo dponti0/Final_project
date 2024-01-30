f388a0d4a52188a0f624376971c564475528c868
dponti0 <155808821+dponti0@users.noreply.github.com>
2024-01-30 13:37:21 +0100

    Cleaning & filtering

---
+++ scripts/data_cleaning.py
@@ -0,0 +1,109 @@
+"""
+Script for cleaning the DataFrame
+"""
+
+import os
+import pandas as pd
+import click
+from sklearn.ensemble import IsolationForest
+
+class DataCleaningClass:
+    """
+    Class for cleaning operations on a DataFrame
+    """
+
+    def __init__(self, df: pd.DataFrame):
+        """
+        Initialize the class with a DataFrame
+        """
+        self.df = df
+
+    def remove_outliers(self, columns: list) -> pd.DataFrame:
+        """
+        Remove outliers from specified columns using Isolation Forest
+        """
+        # Handle missing values (drop rows with missing values for simplicity, adjust as needed)
+        self.df = self.df.dropna(subset=columns)
+
+        clf = IsolationForest(contamination=0.05)  
+        outliers_mask = clf.fit_predict(self.df[columns]) == -1
+        self.df = self.df[~outliers_mask]
+        return self.df
+
+    def remove_duplicates(self) -> pd.DataFrame:
+        """
+        Remove duplicate rows from the DataFrame
+        """
+        self.df = self.df.drop_duplicates()
+        return self.df
+
+    def handle_missing_values(self) -> pd.DataFrame:
+        """
+        Handle missing values by dropping or imputing as needed
+        """
+        self.df = self.df.dropna()
+        return self.df
+    
+    def create_age_group_column(self):
+        """
+        Create a new column 'age_group' in the DataFrame based on age grouping in 10s.
+        """
+        self.df['age_group'] = ((self.df['age'] // 10) * 10).astype(int)
+
+# Click commands
+@click.command(short_help="Clean and filter dataset")
+@click.option("-i", "--input", default="healthcare_dataset.csv", required=True, help="Input CSV file path")
+@click.option("-o", "--output", default="cleaned_healthcare_dataset.csv", help="Output CSV file path")
+def clean_and_filter_dataset(input, output):
+    """
+    Commands to clean and filter a healthcare dataset.
+    """
+    # Load the dataset into a Pandas DataFrame
+    health_data = pd.read_csv(input)
+
+    # Print the shape of the original dataset
+    print("Original dataset shape:", health_data.shape)
+
+    # Initialize the cleaning class
+    cleaning_instance = DataCleaningClass(health_data)
+
+    # Remove outliers from specific columns
+    columns_with_outliers = ['age', 'avg_glucose_level', 'bmi']
+    cleaned_data = cleaning_instance.remove_outliers(columns_with_outliers)
+
+    # Remove duplicates
+    cleaned_data = cleaning_instance.remove_duplicates()
+
+    # Handle missing values
+    cleaned_data = cleaning_instance.handle_missing_values()
+
+    # Create a new column 'age_group' based on age grouping in 10s
+    cleaning_instance.create_age_group_column()
+
+    # Print the shape of the cleaned dataset
+    print("Cleaned dataset shape:", cleaned_data.shape)
+
+    # Check if the cleaned dataset has any missing values
+    if cleaned_data.isnull().values.any():
+        print("The cleaned dataset contains missing values")
+    else:
+        print("The cleaned dataset has no missing values")
+
+    # Check if the cleaned dataset has any duplicates
+    if cleaned_data.duplicated().any():
+        print("The cleaned dataset contains duplicates")
+    else:
+        print("The cleaned dataset has no duplicates either")
+
+    # Save the cleaned and filtered dataset to the specified output file
+    os.makedirs(os.path.dirname(output), exist_ok=True)     # Create the output folder if it doesn't exist
+    cleaned_data.to_csv(output, index=False)
+    print(f"The cleaned version of the dataset was saved to {output}")
+
+if __name__ == "__main__":
+    print("The code is properly working!")
+    clean_and_filter_dataset()
+
+
+# python scripts/data_cleaning.py -i dataset/healthcare_dataset.csv -o outputs/cleaned_dataset.csv
+    
--- scripts/filtering.py
+++ scripts/filtering.py
@@ -1,4 +1,48 @@
 """
-Code for filtering
+Script to filter the dataframe by desease
 """
+
 import pandas as pd
+
+class FilteringClass:
+    """
+    Class for filtering operations  
+    """
+
+    def __init__(self, df: pd.DataFrame):
+        """
+        Initialize the class with a DataFrame
+        """
+        self.df = df
+
+    def filter_by_hypertension(self) -> pd.DataFrame:
+        """
+        Filter the DataFrame to keep only individuals with hypertension
+        """
+        return self.df[self.df['hypertension'] == 1]
+
+    def filter_by_heart_disease(self) -> pd.DataFrame:
+        """
+        Filter the DataFrame to keep only individuals with heart disease
+        """
+        return self.df[self.df['heart_disease'] == 1]
+
+    def filter_by_high_glucose(self, glucose_threshold: float) -> pd.DataFrame:
+        """
+        Filter the DataFrame to keep only individuals with high glucose levels
+        """
+        glucose_threshold = 125
+        return self.df[self.df['avg_glucose_level'] > glucose_threshold]
+
+    def filter_by_high_bmi(self, bmi_threshold: float) -> pd.DataFrame:
+        """
+        Filter the DataFrame to keep only individuals with high BMI
+        """
+        bmi_threshold = 30    # Obesity
+        return self.df[self.df['bmi'] > bmi_threshold]
+
+    def filter_by_stroke(self) -> pd.DataFrame:
+        """
+        Filter the DataFrame to keep only individuals who had a stroke
+        """
+        return self.df[self.df['stroke'] == 1]
