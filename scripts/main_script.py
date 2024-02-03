0eb0bcc84fc8e297781ddd883f30990769f3485e
dponti0 <155808821+dponti0@users.noreply.github.com>
2024-02-03 22:28:57 +0100

    Latest General Updates

--- scripts/main_script.py
+++ scripts/main_script.py
@@ -27,33 +27,43 @@ def main():
     bi_visualizer = MultivariateVisualizer(data, bi_pdf_pages)
 
     # Llamar a los métodos de visualización (univariate)
-    uni_visualizer.visualize_age_distribution()
-    uni_visualizer.visualize_age_group_distribution()
-    uni_visualizer.visualize_gender_distribution()
-    uni_visualizer.visualize_hypertension_distribution()
-    uni_visualizer.visualize_heart_disease_distribution()
-    uni_visualizer.visualize_strokes_distribution()
-    uni_visualizer.visualize_glucose_distribution()
-    uni_visualizer.visualize_bmi_distribution()
-    uni_visualizer.visualize_smoking_distribution()
-    uni_visualizer.visualize_work_type_distribution()
-    uni_visualizer.visualize_marital_status_distribution()
-    uni_visualizer.visualize_residence_type_distribution()
 
-    # Llamar a los métodos de visualización (multivariate)
-    bi_visualizer.age_stroke_distribution()
-    bi_visualizer.glucose_stroke_distribution()
-    bi_visualizer.bmi_stroke_distribution()
-    bi_visualizer.bmi_glucose_scatter()
-    bi_visualizer.heart_disease_gender_age()
-    bi_visualizer.glucose_smoking_distribution()
-    bi_visualizer.age_bmi_relation()
-    bi_visualizer.work_type_stroke_distribution()
-    bi_visualizer.marriage_glucose_relation()
-    bi_visualizer.marriage_bmi_relation()
-    bi_visualizer.age_stroke_rate_lineplot()
-    bi_visualizer.correlation_heatmap()
+    def visualize_univariate_relationships(uni_visualizer):
+        """
+        Visualize univariate distributions using the provided UniVisualizer instance
+        """  
+        uni_visualizer.visualize_age_distribution()
+        uni_visualizer.visualize_age_group_distribution()
+        uni_visualizer.visualize_gender_distribution()
+        uni_visualizer.visualize_hypertension_distribution()
+        uni_visualizer.visualize_heart_disease_distribution()
+        uni_visualizer.visualize_strokes_distribution()
+        uni_visualizer.visualize_glucose_distribution()
+        uni_visualizer.visualize_bmi_distribution()
+        uni_visualizer.visualize_smoking_distribution()
+        uni_visualizer.visualize_work_type_distribution()
+        uni_visualizer.visualize_marital_status_distribution()
+        uni_visualizer.visualize_residence_type_distribution()
 
+    def visualize_multivariate_relationships(bi_visualizer):
+        """
+        Visualize multivariate distributions using the provided MultiVisualizer instance
+        """
+        bi_visualizer.age_stroke_distribution()
+        bi_visualizer.glucose_stroke_distribution()
+        bi_visualizer.bmi_stroke_distribution()
+        bi_visualizer.bmi_glucose_scatter()
+        bi_visualizer.heart_disease_gender_age()
+        bi_visualizer.glucose_smoking_distribution()
+        bi_visualizer.age_bmi_relation()
+        bi_visualizer.work_type_stroke_distribution()
+        bi_visualizer.marriage_glucose_relation()
+        bi_visualizer.marriage_bmi_relation()
+        bi_visualizer.age_stroke_rate_lineplot()
+        bi_visualizer.correlation_heatmap()
+
+    visualize_univariate_relationships(uni_visualizer)
+    visualize_multivariate_relationships(bi_visualizer)
 
     # Cerrar los objetos PdfPages para finalizar los PDFs
     uni_pdf_pages.close()
--- scripts/univariate_analysis.py
+++ scripts/univariate_analysis.py
@@ -16,116 +16,225 @@ class UnivariateVisualizer:
     def visualize_age_distribution(self):
         plt.figure(figsize=(10, 6))
         sns.histplot(self.df["age"], bins=20, kde=True, color="skyblue")
-        plt.title("Age Distribution")
-        plt.xlabel("Age")
-        plt.ylabel("Frequency")
+        plt.title("Age Distribution", weight='bold')
+        plt.xlabel("Age", fontstyle='italic')
+        plt.ylabel("Frequency", fontstyle='italic')
+        sns.despine()
         self.save_and_close()
 
     def visualize_age_group_distribution(self):
+        # Crea el gráfico
         plt.figure(figsize=(10, 6))
-        sns.countplot(x="age_group", data=self.df, palette="muted")
-        plt.title("Age Group Distribution")
-        plt.xlabel("Age Group")
-        plt.ylabel("Number of People")
+        ax = sns.histplot(x="age_group", data=self.df, bins=20, kde=True, color="skyblue")
+
+        # Ajusta el título superior en negrita
+        plt.title("Age Group Distribution", weight='bold')
+
+        # Ajusta los títulos laterales en cursiva
+        ax.set_xlabel("Age Group", fontstyle='italic')
+        ax.set_ylabel("Frequency", fontstyle='italic')
+
+        # Elimina el marco del gráfico
+        sns.despine()
+
         self.save_and_close()
 
     def visualize_gender_distribution(self):
-        plt.figure(figsize=(8, 5))
-        sns.countplot(x="gender", data=self.df, palette="pastel")
-        plt.title("Gender Distribution")
-        plt.xlabel("Gender")
-        plt.ylabel("Number of People")
+        f_df = self.df[self.df['gender'].isin(['male', 'female'])]
+        total_samples = len(f_df)
+        gender_percentages = f_df['gender'].value_counts(normalize=True) * 100
+
+        # Configure the style
+        sns.set(style="whitegrid")
+        plt.figure(figsize=(7, 5))
+
+        # Create the bar plot
+        ax = sns.barplot(x=gender_percentages.values, y=gender_percentages.index, palette="pastel")
+
+        # Add percentages on the vertical axis
+        for i, percentage in enumerate(gender_percentages):
+            ax.text(percentage + 1, i, f'{percentage:.0f}%', va='center', fontweight='bold')
+
+        # Adjust title and labels
+        plt.title("Gender Distribution", weight='bold')
+        plt.ylabel("Gender", fontstyle='italic', fontsize=9)
+        plt.xticks([])
+
+        sns.despine(bottom=True)
         self.save_and_close()
 
     def visualize_hypertension_distribution(self):
-        plt.figure(figsize=(8, 5))
-        ax = sns.countplot(x="hypertension", data=self.df, palette="Set2")
-        plt.title("Hypertension Distribution")
-        plt.xlabel("Hypertension")
-        plt.ylabel("Number of People")
-        ax.set_xticklabels(["No", "Yes"])
+        # Crea el gráfico de pastel
+        plt.figure(figsize=(8, 8))
+        hypertension_counts = self.df['hypertension'].value_counts()
+        colors = sns.color_palette("pastel")
 
-        # Agrega el recuento en la leyenda
-        counts = self.df["hypertension"].value_counts()
-        for i, count in enumerate(counts):
-            ax.text(i, count + 1, str(count), ha='center')
+        # Utiliza solo una variable para obtener las características del pie chart
+        wedges, _ = plt.pie(hypertension_counts, startangle=90,
+                            colors=colors, wedgeprops=dict(width=0.5), shadow=True)
+
+        # Elimina el marco del gráfico
+        plt.gca().set_aspect('equal')
+
+        # Ajusta el título para que sea en negrita
+        plt.title("Hypertension Distribution", weight='bold')
+
+        # Añade la leyenda con labels personalizados
+        plt.legend(labels=["No", "Yes"], loc="upper right", bbox_to_anchor=(1, 0.8), title="Hypertension")
 
         self.save_and_close()
+
 
     def visualize_heart_disease_distribution(self):
-        plt.figure(figsize=(8, 5))
-        ax = sns.countplot(x="heart_disease", data=self.df, palette="Set2")
-        plt.title("Heart Disease Distribution")
-        plt.xlabel("Heart Disease")
-        plt.ylabel("Number of People")
-        ax.set_xticklabels(["No", "Yes"])
+        # Crea el gráfico de pastel
+        plt.figure(figsize=(8, 8))
+        heart_disease_counts = self.df['heart_disease'].value_counts()
+        colors = sns.color_palette("pastel")
 
-        # Agrega el recuento en la leyenda
-        counts = self.df["heart_disease"].value_counts()
-        for i, count in enumerate(counts):
-            ax.text(i, count + 1, str(count), ha='center')
+        # Utiliza solo una variable para obtener las características del pie chart
+        _ = plt.pie(heart_disease_counts, startangle=90,
+                    colors=colors, wedgeprops=dict(width=0.5), labeldistance=1.2, shadow=True)
 
-        self.save_and_close()
+        # Elimina el marco del gráfico
+        plt.gca().set_aspect('equal')
 
-    def visualize_strokes_distribution(self):
-        plt.figure(figsize=(8, 5))
-        ax = sns.countplot(x="stroke", data=self.df, palette="Set2")
-        plt.title("Stroke Distribution")
-        plt.xlabel("Stroke")
-        plt.ylabel("Number of People")
-        ax.set_xticklabels(["No", "Yes"])
+        # Ajusta el título para que sea en negrita
+        plt.title("Heart Disease Distribution", weight='bold')
 
-        # Agrega el recuento en la leyenda
-        counts = self.df["stroke"].value_counts()
-        for i, count in enumerate(counts):
-            ax.text(i, count + 1, str(count), ha='center')
+        # Añade la leyenda con labels personalizados
+        plt.legend(labels=["No", "Yes"], loc="upper right", bbox_to_anchor=(1, 0.8), title="Heart Disease")
 
         self.save_and_close()
 
     def visualize_glucose_distribution(self):
         plt.figure(figsize=(10, 6))
         sns.histplot(self.df["avg_glucose_level"], bins=20, kde=True, color="orange")
-        plt.title("Glucose Levels Distribution")
-        plt.xlabel("Glucose Level")
-        plt.ylabel("Frequency")
+        plt.title("Glucose Levels Distribution", weight='bold')
+        plt.xlabel("Glucose Level", fontstyle='italic')
+        plt.ylabel("Frequency", fontstyle='italic')
+        sns.despine()
         self.save_and_close()
 
     def visualize_bmi_distribution(self):
         plt.figure(figsize=(10, 6))
         sns.histplot(self.df["bmi"].dropna(), bins=20, kde=True, color="green")
-        plt.title("Body Mass Index (BMI) Distribution")
-        plt.xlabel("BMI")
-        plt.ylabel("Frequency")
+        plt.title("Body Mass Index (BMI) Distribution", weight='bold')
+        plt.xlabel("BMI", fontstyle='italic')
+        plt.ylabel("Frequency", fontstyle='italic')
+        sns.despine()
         self.save_and_close()
 
-    def visualize_smoking_distribution(self):
-        plt.figure(figsize=(8, 5))
-        sns.countplot(x="smoking_status", data=self.df, palette="pastel")
-        plt.title("Smoking Distribution")
-        plt.xlabel("Smoking Status")
-        plt.ylabel("Number of People")
-        self.save_and_close()
+
 
     def visualize_work_type_distribution(self):
+        # Establece el estilo de seaborn
+        sns.set_style("whitegrid")
+
+        # Configura la paleta de colores
+        palette = sns.color_palette("Blues_d")
+
+        # Crea el gráfico
         plt.figure(figsize=(10, 6))
-        sns.countplot(x="work_type", data=self.df, palette="viridis")
-        plt.title("Work Type Distribution")
-        plt.xlabel("Work Type")
-        plt.ylabel("Number of People")
+        ax = sns.countplot(x="work_type", data=self.df, palette=palette, edgecolor="none")
+
+        # Añade líneas horizontales grises
+        ax.yaxis.grid(True, linestyle='--', alpha=0.7, color='gray')
+
+        # Ajusta el título superior en negrita
+        plt.title("Work Type Distribution", weight='bold')
+
+        # Ajusta los títulos laterales en cursiva
+        ax.set_xlabel("Work Type", fontstyle='italic', fontsize=10)
+        ax.set_ylabel("Number of People", fontstyle='italic', fontsize=10)
+
+        # Elimina el marco del gráfico
+        sns.despine()
+
         self.save_and_close()
 
+
+
     def visualize_marital_status_distribution(self):
-        plt.figure(figsize=(8, 5))
-        sns.countplot(x="ever_married", data=self.df, palette="pastel")
-        plt.title("Marital Status Distribution")
-        plt.xlabel("Marital Status")
-        plt.ylabel("Number of People")
+        plt.figure(figsize=(7, 7))
+        marital_status_counts = self.df['ever_married'].value_counts()
+        colors = sns.color_palette("pastel")
+
+        _ = plt.pie(marital_status_counts, startangle=90, colors=colors, wedgeprops=dict(width=0.5), shadow=True)
+
+        # Elimina el marco del gráfico
+        plt.gca().set_aspect('equal')
+        
+        # Ajusta el título para que sea en negrita
+        plt.title("Marital Status Distribution", weight='bold')
+
+        # Agrega un label
+        plt.legend(labels=marital_status_counts.index, loc="upper right", bbox_to_anchor=(1, 0.8), title="Marital Status")
+
         self.save_and_close()
 
     def visualize_residence_type_distribution(self):
+        plt.figure(figsize=(7, 7))
+        residence_type_counts = self.df['Residence_type'].value_counts()
+        colors = sns.color_palette("pastel")
+
+        wedges, _ = plt.pie(residence_type_counts, startangle=90, colors=colors, wedgeprops=dict(width=0.4), 
+                            textprops=dict(weight='bold'), labeldistance=1.2, shadow=True)  # Añade shadow=True para sombras
+
+        # Elimina el marco del gráfico
+        plt.gca().set_aspect('equal')
+        
+        # Ajusta el título para que sea en negrita
+        plt.title("Residence Type Distribution", weight='bold')
+
+        # Agrega un label
+        plt.legend(labels=residence_type_counts.index, loc="upper right", bbox_to_anchor=(1, 0.8), title="Residence Type")
+
+        self.save_and_close()
+
+
+    def visualize_smoking_distribution(self):
+        # Establece el estilo de seaborn
+        sns.set_style("whitegrid")
+
+        # Configura la paleta de colores
+        palette = sns.color_palette("Blues_d")
+
+        # Crea el gráfico
         plt.figure(figsize=(8, 5))
-        sns.countplot(x="Residence_type", data=self.df, palette="pastel")
-        plt.title("Residence Type Distribution")
-        plt.xlabel("Residence Type")
-        plt.ylabel("Number of People")
+        ax = sns.countplot(x="smoking_status", data=self.df, palette=palette, edgecolor="none")
+
+        # Añade líneas horizontales grises
+        ax.yaxis.grid(True, linestyle='--', alpha=0.7, color='gray')
+
+        # Ajusta el título superior en negrita
+        plt.title("Smoking Distribution", weight='bold')
+
+        # Ajusta los títulos laterales en cursiva
+        ax.set_xlabel("Smoking Status", fontstyle='italic')
+        ax.set_ylabel("Number of People", fontstyle='italic')
+
+        # Elimina el marco del gráfico
+        sns.despine()
+
+        self.save_and_close()
+
+
+    def visualize_strokes_distribution(self):
+        # Crea el gráfico de pastel
+        plt.figure(figsize=(8, 8))
+        strokes_counts = self.df['stroke'].value_counts()
+        colors = sns.color_palette("pastel")
+
+        # Utiliza solo una variable para obtener las características del pie chart
+        _ = plt.pie(strokes_counts, startangle=90,
+                    colors=colors, wedgeprops=dict(width=0.4), labeldistance=1.2, shadow=True)
+        
+        # Elimina el marco del gráfico
+        plt.gca().set_aspect('equal')
+        
+        # Ajusta el título para que sea en negrita
+        plt.title("Stroke Distribution", weight='bold')
+
+        plt.legend(labels=["Yes", "No"], loc="upper right", bbox_to_anchor=(1, 0.8), title="Stroke")
+
         self.save_and_close()
