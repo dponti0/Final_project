9c8d8a8ae5fc5b0313abe6a6de430e334ecad852
dponti0 <155808821+dponti0@users.noreply.github.com>
2024-02-03 14:17:10 +0100

    Univariate Analysis Latest Changes

--- scripts/main_script.py
+++ scripts/main_script.py
@@ -4,7 +4,7 @@
 # main_script.py
 import pandas as pd
 import matplotlib.backends.backend_pdf as pdf_backend
-from univariate_analysis import visualize_age_distribution, visualize_gender_distribution, visualize_hypertension_distribution, visualize_heart_disease_distribution, visualize_strokes_distribution, visualize_glucose_distribution, visualize_bmi_distribution, visualize_smoking_distribution, visualize_work_type_distribution, visualize_marital_status_distribution, visualize_residence_type_distribution
+from univariate_analysis import visualize_age_distribution, visualize_age_group_distribution, visualize_gender_distribution, visualize_hypertension_distribution, visualize_heart_disease_distribution, visualize_strokes_distribution, visualize_glucose_distribution, visualize_bmi_distribution, visualize_smoking_distribution, visualize_work_type_distribution, visualize_marital_status_distribution, visualize_residence_type_distribution
 from filtering import FilteringClass
 import os
 
@@ -22,17 +22,13 @@ def main():
     pdf_path = "outputs/univariate_analysis.pdf"
     pdf_pages = pdf_backend.PdfPages(pdf_path)
 
-    # Filtrar por hipertensión, enfermedad cardíaca y strokes
-    hypertension_data = filter_instance.filter_by_hypertension()
-    heartdis_data = filter_instance.filter_by_heart_disease()
-    strokes_data = filter_instance.filter_by_stroke()
-
     # Llamar a funciones para visualizar y guardar los gráficos en la carpeta 'outputs'
     visualize_age_distribution(data, "outputs/age_distribution_plot.png", pdf_pages)
+    visualize_age_group_distribution(data, "outputs/age_group_plot.png", pdf_pages)
     visualize_gender_distribution(data, "outputs/gender_distribution_plot.png", pdf_pages)
-    visualize_hypertension_distribution(hypertension_data, "outputs/hypertension_plot.png", pdf_pages)
-    visualize_heart_disease_distribution(heartdis_data, "outputs/heartdisease_plot.png", pdf_pages)
-    visualize_strokes_distribution(strokes_data, "outputs/strokes_plot.png", pdf_pages)
+    visualize_hypertension_distribution(data, "outputs/hypertension_plot.png", pdf_pages)
+    visualize_heart_disease_distribution(data, "outputs/heartdisease_plot.png", pdf_pages)
+    visualize_strokes_distribution(data, "outputs/strokes_plot.png", pdf_pages)
     visualize_glucose_distribution(data, "outputs/glucose_distribution_plot.png", pdf_pages)
     visualize_bmi_distribution(data, "outputs/bmi_distribution_plot.png", pdf_pages)
     visualize_smoking_distribution(data, "outputs/smoking_distribution_plot.png", pdf_pages)
--- scripts/univariate_analysis.py
+++ scripts/univariate_analysis.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 def visualize_age_distribution(df, save_path, pdf_pages=None):
     plt.figure(figsize=(10, 6))
     sns.histplot(df["age"], bins=20, kde=True, color="skyblue")
@@ -11,6 +12,15 @@ def visualize_age_distribution(df, save_path, pdf_pages=None):
     plt.ylabel("Frequency")
     pdf_pages.savefig()  
     plt.close()  
+
+def visualize_age_group_distribution(df, save_path, pdf_pages=None):
+    plt.figure(figsize=(10, 6))
+    sns.countplot(x="age_group", data=df, palette="muted")
+    plt.title("Age Group Distribution")
+    plt.xlabel("Age Group")
+    plt.ylabel("Number of People")
+    pdf_pages.savefig()
+    plt.close()
 
 def visualize_gender_distribution(df, save_path, pdf_pages=None):
     plt.figure(figsize=(8, 5))
@@ -21,26 +31,51 @@ def visualize_gender_distribution(df, save_path, pdf_pages=None):
     pdf_pages.savefig()  
     plt.close()  
 
+
 def visualize_hypertension_distribution(df, save_path, pdf_pages=None):
     plt.figure(figsize=(8, 5))
-    sns.countplot(x="hypertension", data=df, palette="Set2")
+    ax = sns.countplot(x="hypertension", data=df, palette="Set2")
     plt.title("Hypertension Distribution")
-    pdf_pages.savefig()  
-    plt.close()  
+    plt.xlabel("Hypertension")
+    plt.ylabel("Number of People")
+
+    # Agrega el recuento en la leyenda
+    counts = df["hypertension"].value_counts()
+    for i, count in enumerate(counts):
+        ax.text(i, count + 1, str(count), ha='center')
+
+    pdf_pages.savefig()
+    plt.close()
 
 def visualize_heart_disease_distribution(df, save_path, pdf_pages=None):
     plt.figure(figsize=(8, 5))
-    sns.countplot(x="heart_disease", data=df, palette="Set2")
+    ax = sns.countplot(x="heart_disease", data=df, palette="Set2")
     plt.title("Heart Disease Distribution")
-    pdf_pages.savefig()  
-    plt.close()  
+    plt.xlabel("Heart Disease")
+    plt.ylabel("Number of People")
+
+    # Agrega el recuento en la leyenda
+    counts = df["heart_disease"].value_counts()
+    for i, count in enumerate(counts):
+        ax.text(i, count + 1, str(count), ha='center')
+
+    pdf_pages.savefig()
+    plt.close()
 
 def visualize_strokes_distribution(df, save_path, pdf_pages=None):
     plt.figure(figsize=(8, 5))
-    sns.countplot(x="stroke", data=df, palette="Set2")
+    ax = sns.countplot(x="stroke", data=df, palette="Set2")
     plt.title("Stroke Distribution")
-    pdf_pages.savefig()  
-    plt.close()  
+    plt.xlabel("Stroke")
+    plt.ylabel("Number of People")
+
+    # Agrega el recuento en la leyenda
+    counts = df["stroke"].value_counts()
+    for i, count in enumerate(counts):
+        ax.text(i, count + 1, str(count), ha='center')
+
+    pdf_pages.savefig()
+    plt.close()
 
 def visualize_glucose_distribution(df, save_path, pdf_pages=None):
     plt.figure(figsize=(10, 6))
